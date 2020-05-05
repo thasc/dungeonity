5171d7050fb3a8b65aa0c73d0eb3066fc57bfc23
i forgot to set this <definitelymyrealaccount@pm.me>
2020-05-05 19:06:09 +0100

    Fixup collider and photographer to match tilemap.

--- Assets/Scripts/MainTileMapInitialiser.cs
+++ Assets/Scripts/MainTileMapInitialiser.cs
@@ -7,20 +7,34 @@ public class MainTileMapInitialiser : MonoBehaviour
     public Tile InitialTile;
     
     private DungeonStateManager m_DungeonStateManager;
+    private Tilemap m_TileMap;
+    private BoxCollider2D m_Collider;
 
     private void Awake()
     {
         m_DungeonStateManager = GameObject.FindWithTag("DungeonStateManager")?
                                     .GetComponent<DungeonStateManager>() ??
                                 throw new ArgumentNullException(nameof(m_DungeonStateManager));
+        m_TileMap = GetComponent<Tilemap>();
+        m_Collider = GetComponent<BoxCollider2D>();
     }
 
     private void Start()
     {
-        var tileMap = GetComponent<Tilemap>();
-        tileMap.origin = Vector3Int.zero;
-        tileMap.size = new Vector3Int(m_DungeonStateManager.Width, m_DungeonStateManager.Height, 0);
-        tileMap.ResizeBounds();
-        tileMap.FloodFill(new Vector3Int(0, 0, 0), InitialTile);
+        ResizeTileMap(m_DungeonStateManager.Width, m_DungeonStateManager.Height);
+        m_TileMap.FloodFill(new Vector3Int(0, 0, 0), InitialTile);
+    }
+
+    private void ResizeTileMap(int width, int height)
+    {
+        m_TileMap.origin = Vector3Int.zero;
+        
+        m_TileMap.size = new Vector3Int(width, height, 0);
+        m_TileMap.ResizeBounds();
+
+        var tileMapSize = new Vector2(m_TileMap.size.x, m_TileMap.size.y);
+        
+        m_Collider.size = tileMapSize;
+        m_Collider.offset = tileMapSize / 2;
     }
 }
--- Assets/Scripts/Photographer.cs
+++ Assets/Scripts/Photographer.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Photographer : MonoBehaviour
 {
     private Camera m_Camera;
+    private DungeonStateManager m_DungeonStateManager;
 
     private void Awake()
     {
+        m_DungeonStateManager = GameObject.FindWithTag("DungeonStateManager")
+                                    ?.GetComponent<DungeonStateManager>() ??
+                                throw new ArgumentNullException(nameof(m_DungeonStateManager));
+        
         m_Camera = gameObject.AddComponent<Camera>();
         // set m_Camera.aspect to determine its aspect ratio, and orthographicSize for size
         m_Camera.tag = "PhotographerCamera";
@@ -26,13 +31,7 @@ public class Photographer : MonoBehaviour
 
     private void Snap()
     {
-        var mainCamera = Camera.main;
-        mainCamera.enabled = false;
-        m_Camera.enabled = true;
-        
-        // todo: adjust camera to capture the whole map to an adequately sized render texture 
-    
-        var image = RenderCameraView(m_Camera);
+        var image = RenderCameraView();
         
         // todo: crop image to the correct size for export
         
@@ -42,25 +41,38 @@ public class Photographer : MonoBehaviour
         var captureFolder = Path.Combine(Application.dataPath, "Captures");
         Directory.CreateDirectory(captureFolder);
         File.WriteAllBytes(Path.Combine(captureFolder, DateTime.UtcNow.Ticks + ".png"), bytes);
-
-        m_Camera.enabled = false;
-        mainCamera.enabled = true;
     }
 
-    private static Texture2D RenderCameraView(Camera camera)
+    private Texture2D RenderCameraView()
     {
-        var targetTexture = RenderTexture.GetTemporary(1024, 1024);
-        var suspendedRenderTexture = RenderTexture.active;
-        RenderTexture.active = camera.targetTexture = targetTexture;
+        const int pixelsPerTile = 64;
         
-        camera.Render();
+        var dungeonWidthInPixels = m_DungeonStateManager.Width * pixelsPerTile;
+        var dungeonHeightInPixels = m_DungeonStateManager.Height * pixelsPerTile;
+        
+        var mainCamera = Camera.main;
+        mainCamera.enabled = false;
+        m_Camera.enabled = true;
+        
+        var suspendedRenderTexture = RenderTexture.active;
+        //var targetTexture = new RenderTexture(1024, 1024, 16);
+        //targetTexture.Create();
+        var targetTexture = RenderTexture.GetTemporary(1024, 1024);
+        RenderTexture.active = m_Camera.targetTexture = targetTexture;
+        
+        m_Camera.transform.position = new Vector3(m_DungeonStateManager.Width/2f, m_DungeonStateManager.Height/2f, -10);
+        m_Camera.Render();
         var image = new Texture2D(targetTexture.width, targetTexture.height);
         image.ReadPixels(new Rect(0, 0, targetTexture.width, targetTexture.height), 0, 0);
         image.Apply();
 
-        camera.targetTexture = null;
+        m_Camera.targetTexture = null;    
         RenderTexture.active = suspendedRenderTexture;
+        //targetTexture.Release();
         RenderTexture.ReleaseTemporary(targetTexture);
+        
+        m_Camera.enabled = false;
+        mainCamera.enabled = true;
         
         return image;
     }
