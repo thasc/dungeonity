886b908e8c2ebf82ec58c9d9cd90a33289b2a7e3
i forgot to set this <definitelymyrealaccount@pm.me>
2020-05-06 18:49:22 +0100

    Add generated tiles. Make them join up nicely.

--- Assets/Scripts/TilePainter.cs
+++ Assets/Scripts/TilePainter.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using Code;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TilePainter : MonoBehaviour
 {
-    public Tile ToPaint;
+    public Tile OpenFloorTile;
+    public Tilemap WallTilemap;
     
-    private Tilemap m_Tilemap;
+    private Tilemap m_FloorTilemap;
     private Camera m_MainCamera;
     
     private static readonly Vector3Int s_DragStartNullObject = new Vector3Int(int.MaxValue, int.MaxValue, int.MaxValue);
@@ -17,7 +19,7 @@ public class TilePainter : MonoBehaviour
 
     private void Awake()
     {
-        m_Tilemap = GetComponent<Tilemap>() ??
+        m_FloorTilemap = GetComponent<Tilemap>() ??
                     throw new InvalidOperationException(
                         $"{nameof(TilePainter)} must be the sibling of a {nameof(Tilemap)}");
         m_MainCamera = Camera.main;
@@ -30,7 +32,7 @@ public class TilePainter : MonoBehaviour
     {
         if (m_DragStart != s_DragStartNullObject)
         {
-            var tileUnderMouse = m_Tilemap.TileUnderMouse(m_MainCamera);
+            var tileUnderMouse = m_FloorTilemap.TileUnderMouse(m_MainCamera);
             if (tileUnderMouse != m_LastDragPosition)
             {
                 m_DragSelectionRenderer.Draw(m_DragStart, tileUnderMouse);
@@ -41,14 +43,71 @@ public class TilePainter : MonoBehaviour
 
     private void OnMouseDown()
     {
-        m_DragStart = m_Tilemap.TileUnderMouse(m_MainCamera);
+        m_DragStart = m_FloorTilemap.TileUnderMouse(m_MainCamera);
     }
 
     private void OnMouseUp()
     {
         m_DragSelectionRenderer.Clear();
-        m_Tilemap.BetterBoxFill(ToPaint, m_DragStart, m_Tilemap.TileUnderMouse(m_MainCamera));
+        var endPosition = m_FloorTilemap.TileUnderMouse(m_MainCamera);
+        m_FloorTilemap.BetterBoxFill(OpenFloorTile, m_DragStart, endPosition);
+
+        var cornerOne = new Vector2Int(Math.Min(m_DragStart.x, endPosition.x), Math.Min(m_DragStart.y, endPosition.y));
+        var cornerTwo = new Vector2Int(Math.Max(m_DragStart.x, endPosition.x), Math.Max(m_DragStart.y, endPosition.y));
+        
+        Debug.Log($"Writing tiles from {cornerOne} to {cornerTwo}");
+        
+        for (var y = cornerOne.y - 1; y < cornerTwo.y + 2; y++)
+        for (var x = cornerOne.x - 1; x < cornerTwo.x + 2; x++)
+        {
+            if (x >= 0 && x <= m_FloorTilemap.size.x && y >= 0 && y <= m_FloorTilemap.size.y)
+            {
+                UpdateTile(x, y);
+            }
+        }
+        
         m_DragStart = s_DragStartNullObject;
         m_LastDragPosition = s_DragStartNullObject;
+    }
+
+    private void UpdateTile(int x, int y)
+    {
+        var tileIndex = 0;
+        
+        var thisTile = new Vector3Int(x, y, 0);
+
+        if (!IsTileOpen(thisTile))
+        {
+            return; // only want to adjust walls for open tiles, for this exact moment
+            // might change later when we start painting tiles closed
+        }
+
+        var northWestOpen = IsTileOpen(thisTile + Vector3Int.up + Vector3Int.left);
+        var northOpen = IsTileOpen(thisTile + Vector3Int.up);
+        var northEastOpen = IsTileOpen(thisTile + Vector3Int.up + Vector3Int.right);
+        
+        var westOpen = IsTileOpen(thisTile + Vector3Int.left);
+        var eastOpen = IsTileOpen(thisTile + Vector3Int.right);
+        
+        var southWestOpen = IsTileOpen(thisTile + Vector3Int.down + Vector3Int.left);
+        var southOpen = IsTileOpen(thisTile + Vector3Int.down);
+        var southEastOpen = IsTileOpen(thisTile + Vector3Int.down + Vector3Int.right);
+
+        if (northWestOpen) tileIndex += 1;
+        if (northOpen) tileIndex += 2;
+        if (northEastOpen) tileIndex += 4;
+        if (westOpen) tileIndex += 8;
+        if (eastOpen) tileIndex += 16;
+        if (southWestOpen) tileIndex += 32;
+        if (southOpen) tileIndex += 64;
+        if (southEastOpen) tileIndex += 128;
+
+        var tile = AssetDatabase.LoadAssetAtPath<Tile>($"Assets/Tiles/Generated/wallsandcorners_{tileIndex}.asset");
+        WallTilemap.SetTile(thisTile, tile);
+    }
+
+    private bool IsTileOpen(Vector3Int tilePos)
+    {
+        return m_FloorTilemap.GetTile<Tile>(tilePos) == OpenFloorTile;
     }
 }
